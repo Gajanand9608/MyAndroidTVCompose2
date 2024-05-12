39f427394ce53fe459f3949e4c353197e9b7661a
Gajanand <gajanand@Gajanands-MacBook-Pro.local>
2024-05-12 12:57:33 +0530

    check 4

--- app/src/main/java/com/example/mytvapplication/Constants.kt
+++
@@ -1,6 +0,0 @@
-package com.example.mytvapplication
-
-object Constants {
-    const val database_url = "https://tvapplication-39985-default-rtdb.asia-southeast1.firebasedatabase.app/"
-    const val reference  = "videoUris"
-}
